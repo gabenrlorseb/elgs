0e869b0917c3c14085a1c6186ac5c4461524f331
95seb <95seba@live.it>
2021-01-13 20:24:59 +0100

    Ottimizzazione profilo utente e modifiche alla classe BaseActivity.java; ora non sono più utili le classi ProfileTeacher.java, ProfileStudent.java ed aggiornamento del manifest

--- unijet/src/main/java/com/legs/unijet/Profile.java
+++ unijet/src/main/java/com/legs/unijet/Profile.java
@@ -2,13 +2,18 @@ package com.legs.unijet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,12 +23,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.core.view.View;
 
-public class ProfileTeacher extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
     Button logout_button,settings_button,notifications_button;
 
 
     FirebaseUser user;
     String userId;
+    FirebaseUser auth;
     DatabaseReference reference;
     private View view;
 
@@ -35,18 +41,22 @@ public class ProfileTeacher extends AppCompatActivity {
         logout_button = findViewById (R.id.logout_button);
 
         user = FirebaseAuth.getInstance().getCurrentUser ();
-        reference = FirebaseDatabase.getInstance ().getReference ("teachers");
         userId=user.getUid ();
+        String email=user.getEmail();
+        if(email.contains("@studenti.uniba.it")) {
+            reference = FirebaseDatabase.getInstance ().getReference ("students");
+        }
+        else{
+            reference = FirebaseDatabase.getInstance ().getReference ("teachers");
+        }
         final TextView text_name_surname= (TextView)findViewById (R.id.text_name_surname);
         final TextView email_logn_field=(TextView) findViewById (R.id.email_logn_field);
-
         reference.child(userId).addListenerForSingleValueEvent (new ValueEventListener () {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User userProfile = snapshot.getValue (User.class);
                 if (userProfile != null) {
-
                     String name = userProfile.name;
                     String email = user.getEmail();
 
@@ -54,8 +64,6 @@ public class ProfileTeacher extends AppCompatActivity {
                     String nameFull= name+"  "+surname;
                     text_name_surname.setText (nameFull.toUpperCase ());
                     email_logn_field.setText (email);
-                }else{
-                    Log.e ("login","fuori ondatachange");
                 }
             }
 
@@ -64,6 +72,7 @@ public class ProfileTeacher extends AppCompatActivity {
 
             }
         });
+
 
 
 
@@ -92,5 +101,3 @@ public class ProfileTeacher extends AppCompatActivity {
 
 
 }
-
-
