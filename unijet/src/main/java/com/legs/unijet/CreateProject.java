d5e26de417492bed8c0cb21b9d72069271a5cb94
anzenrico <enrico.anzelmo@gmail.com>
2021-02-01 12:46:35 +0100

    Creazione spinner che acquisisce il nome e l'anno accademico del corso del progetto da Firebase

--- unijet/src/main/java/com/legs/unijet/CreateProject.java
+++ unijet/src/main/java/com/legs/unijet/CreateProject.java
@@ -5,18 +5,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.legs.unijet.createGroupActivity.UserSample;
+
+import java.util.ArrayList;
 
 public class CreateProject extends AppCompatActivity {
     private EditText inputNameProject;
@@ -24,6 +32,7 @@ public class CreateProject extends AppCompatActivity {
     FirebaseAuth auth;
     DatabaseReference db;
     Button btnCreation;
+    ArrayList <String> courses;
 
     Bundle bundle;
     Intent intent;
@@ -41,14 +50,14 @@ public class CreateProject extends AppCompatActivity {
         DatabaseReference reference;
         super.onCreate (savedInstanceState);
         setContentView (R.layout.create_project);
+        populateSpinnerCourse();
+        populateSpinnerGroup();
 
         inputNameProject = findViewById (R.id.set_name_project);
         inputCourse = findViewById (R.id.select_course);
         inputGroup = findViewById (R.id.select_group);
 
-        firebaseAuth = FirebaseAuth.getInstance ();
 
-        db = FirebaseDatabase.getInstance().getReferenceFromUrl("https://myunijet-default-rtdb.firebaseio.com/courses/CI67AHZLu5Z3Teq49qcy1TI3Lz72/2020-2021/Chemstry");
 
 
 
@@ -56,7 +65,7 @@ public class CreateProject extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance ();
         LoadingBar = new ProgressDialog (CreateProject.this);
-        btnCreation = findViewById (R.id.creation_button);
+        btnCreation = findViewById (R.id.creation_button_project);
         btnCreation.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -107,8 +116,7 @@ public class CreateProject extends AppCompatActivity {
                 Log.d ("TAG", "checkCrededentials: " + project.getName ());
             }
             Toast.makeText (this, "success", Toast.LENGTH_SHORT).show ();
-            db.child(project.getGroup()).child(project.getCourse()).
-                    child(project.getName()).setValue (project);
+
 
 
             LoadingBar.setTitle (getString (R.string.project_creation));
@@ -120,6 +128,40 @@ public class CreateProject extends AppCompatActivity {
 
     }
 
+private void populateSpinnerCourse() {
+    db = FirebaseDatabase.getInstance().getReference();
+    courses = new ArrayList<>();
+    db.child("courses").addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                for (DataSnapshot childSnapshot2 : childSnapshot.getChildren()) {
+                    for (DataSnapshot childSnapshot3 : childSnapshot2.getChildren()) {
+
+                        for (DataSnapshot childSnapshot4 : childSnapshot3.getChildren()) {
+
+                            String spinnerName = childSnapshot4.child("name").getValue(String.class) + " "
+                                    + childSnapshot4.child("academicYear").getValue(String.class);
+                            courses.add(spinnerName);
+                        }
+                    }
+                }
+            }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(CreateProject.this, android.R.layout.simple_spinner_item, courses);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        inputCourse.setAdapter(arrayAdapter);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    });
+}
+
+private void populateSpinnerGroup (){
+
+    }
     private void showError(EditText input, String s) {
         input.setError (s);
         input.requestFocus ();
