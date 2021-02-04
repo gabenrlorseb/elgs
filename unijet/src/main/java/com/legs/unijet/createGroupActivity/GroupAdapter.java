6e1743f5239a9a526419c920d3a32049f7b00ca0
Lorenzo <lorenzo.aluisi1997@gmail.com>
2021-02-04 13:03:33 +0100

    Aggiunta ProjectAdapter.java, ProjectsFragment.java e projects_page.xml, questi file sono utili per la visualizzazione di tutti i progetti esistenti sul database.

--- unijet/src/main/java/com/legs/unijet/createGroupActivity/GroupAdapter.java
+++ unijet/src/main/java/com/legs/unijet/createGroupActivity/GroupAdapter.java
@@ -8,30 +8,27 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.legs.unijet.CourseAdapter;
-import com.legs.unijet.CourseSample;
+
 import com.legs.unijet.R;
 
 import java.util.ArrayList;
 
-public class GroupAdapter  extends RecyclerView.Adapter<com.legs.unijet.CourseAdapter.CourseViewHolder> {
+public class GroupAdapter  extends RecyclerView.Adapter<GroupAdapter.GroupViewHolder> {
+
+    private ArrayList<UserSample2> groupsList;
 
 
-     private ArrayList<UserSample2> groupsList;
-
-
-        public static class CourseViewHolder extends RecyclerView.ViewHolder {
+        public static class GroupViewHolder extends RecyclerView.ViewHolder {
 
             public TextView mGroups;
 
 
-            public CourseViewHolder(View itemView) {
+            public GroupViewHolder(View itemView) {
                 super(itemView);
                 mGroups = itemView.findViewById(R.id.group_name);
 
             }
         }
-
         public GroupAdapter(ArrayList<UserSample2> groupsList){
             this.groupsList = groupsList;
         }
@@ -43,15 +40,15 @@ public class GroupAdapter  extends RecyclerView.Adapter<com.legs.unijet.CourseAd
 
 
         @Override
-        public com.legs.unijet.CourseAdapter.CourseViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.courses_sample, viewGroup, false);
-            com.legs.unijet.CourseAdapter.CourseViewHolder cvh = new com.legs.unijet.CourseAdapter.CourseViewHolder (v);
+        public GroupAdapter.GroupViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.groups_sample, viewGroup, false);
+            GroupAdapter.GroupViewHolder cvh = new GroupAdapter.GroupViewHolder (v);
             return cvh;
         }
 
         @Override
-        public void onBindViewHolder(@NonNull com.legs.unijet.CourseAdapter.CourseViewHolder courseViewHolder, int i) {
-            courseViewHolder.mCourses.setText(groupsList.get(i).getText1());
+        public void onBindViewHolder(@NonNull GroupAdapter.GroupViewHolder groupViewHolder, int i) {
+            groupViewHolder.mGroups.setText(groupsList.get(i).getText1());
         }
 
         @Override
