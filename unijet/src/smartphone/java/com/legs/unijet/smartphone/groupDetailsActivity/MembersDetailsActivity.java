e3fb7d9c68d0197ae0f334f4d347887274383fe4
anzenrico <enrico.anzelmo@gmail.com>
2021-02-17 19:50:04 +0100

    Creazione build variants relative agli smartphones e ai tablet con conseguente nuova disposizione dei file

---
+++ unijet/src/smartphone/java/com/legs/unijet/smartphone/groupDetailsActivity/MembersDetailsActivity.java
@@ -0,0 +1,98 @@
+ package com.legs.unijet.smartphone.groupDetailsActivity;
+
+ import android.os.Bundle;
+
+ import androidx.annotation.NonNull;
+ import androidx.appcompat.app.AppCompatActivity;
+ import androidx.appcompat.widget.Toolbar;
+ import androidx.recyclerview.widget.LinearLayoutManager;
+ import androidx.recyclerview.widget.RecyclerView;
+
+ import com.google.firebase.database.DataSnapshot;
+ import com.google.firebase.database.DatabaseError;
+ import com.google.firebase.database.DatabaseReference;
+ import com.google.firebase.database.FirebaseDatabase;
+ import com.google.firebase.database.ValueEventListener;
+ import com.legs.unijet.smartphone.R;
+ import com.legs.unijet.smartphone.createGroupActivity.UserChecklistSample;
+
+ import java.util.ArrayList;
+
+ public class MembersDetailsActivity extends AppCompatActivity {
+
+     private ArrayList<UserChecklistSample> names;
+     private ArrayList<String> passed_names;
+
+     RecyclerView mRecyclerView;
+     private MemberNoChecklistAdapter mAdapter;
+     DatabaseReference db= FirebaseDatabase.getInstance ().getReference ();
+
+     String authorName;
+     String authorMail;
+
+
+     @Override
+    protected void onCreate (Bundle savedInstance) {
+
+        setContentView(R.layout.simple_activity_recyclerview_list);
+
+        Toolbar actionBar = findViewById(R.id.toolbar);
+        actionBar.setTitle("Members");
+
+        Bundle args = getIntent().getExtras();
+
+        passed_names =  (ArrayList<String>) args.getSerializable("groupRecipients");
+        authorName = args.getString("author_name");
+
+        if (authorName.equals("you")) {
+            authorName = getString(R.string.you);
+            authorMail = getString(R.string.admin);
+        } else {
+            authorName = args.getString("author_name");
+            authorMail = args.getString("author");
+        }
+
+        populateList();
+
+        super.onCreate(savedInstance);
+    }
+
+     private void populateList() {
+         names = new ArrayList<>();
+         names.add(new UserChecklistSample(R.drawable.ic_people, authorName, authorMail, false));
+         db.child("students").addValueEventListener(new ValueEventListener() {
+             @Override
+             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                 for (DataSnapshot childSnapshot:snapshot.getChildren()) {
+                     if (passed_names.contains(childSnapshot.child("email").getValue(String.class)) ) {
+                         String namesString = childSnapshot.child("name").getValue(String.class) +
+                                 " " +
+                                 childSnapshot.child("surname").getValue(String.class);
+                         String mail = childSnapshot.child ("email").getValue (String.class);
+                         names.add (new UserChecklistSample(R.drawable.ic_people, namesString, mail, false));
+                     }
+                 }
+
+
+
+                 buildRecyclerView();
+             }
+
+             @Override
+             public void onCancelled(@NonNull DatabaseError error) {
+
+             }
+         });
+
+     }
+
+     private void buildRecyclerView() {
+         mRecyclerView = findViewById(R.id.recyclerview_details);
+         mRecyclerView.setHasFixedSize(true);
+         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+         mAdapter = new MemberNoChecklistAdapter(names);
+         mRecyclerView.setLayoutManager(mLayoutManager);
+         mRecyclerView.setAdapter(mAdapter);
+     }
+
+}
---
+++ unijet/src/smartphone/java/com/legs/unijet/smartphone/registerActivity/RegisterActivityStart.java
@@ -0,0 +1,144 @@
+package com.legs.unijet.smartphone.registerActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.legs.unijet.smartphone.BaseActivity;
+import com.legs.unijet.smartphone.CreateCourse;
+import com.legs.unijet.smartphone.MainActivity;
+import com.legs.unijet.smartphone.R;
+
+public class RegisterActivityStart extends AppCompatActivity {
+    TextView btn;
+
+    private EditText inputPassword, inputEmail, inputConfirmPassword;
+    Button btnNext;
+    FirebaseAuth auth;
+    TextView haveAccount;
+    private ProgressDialog LoadingBar;
+    DatabaseReference dbReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.sign_up);
+
+        inputEmail = findViewById (R.id.set_email);
+        inputPassword = findViewById (R.id.set_password);
+        inputConfirmPassword = findViewById (R.id.set_confirm_password);
+        auth=FirebaseAuth.getInstance () ;
+// Write a message to the database
+
+        LoadingBar= new ProgressDialog (RegisterActivityStart.this);
+        btnNext=findViewById (R.id.next_button);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View v) {
+                checkCrededentials();
+
+
+                //btnNext.setOnClickListener (view -> startActivity (new Intent (RegisterActivityStart.this, LoginActivity.class)));
+            }
+        });
+        auth=FirebaseAuth.getInstance();
+        LoadingBar=new ProgressDialog(RegisterActivityStart.this);
+
+        haveAccount=findViewById(R.id.text_help);
+
+        haveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (RegisterActivityStart.this, BaseActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+    void checkCrededentials() {
+        final String email = inputEmail.getText ().toString ();
+        String password = inputPassword.getText ().toString ();
+        String confirmPassword = inputConfirmPassword.getText ().toString ();
+        Log.d ("", "checkCrededentials: email = " + email);
+        Log.d ("", "checkCrededentials: email empty = " + email.isEmpty ());
+
+        Log.d ("", "checkCrededentials: contains studentiuniba = " + !email.contains ("@studenti.uniba.it"));
+
+        Log.d ("", "checkCrededentials: contains uniba = " + !email.contains ("@uniba.it"));
+
+        if (email.isEmpty () && (!email.contains ("@studenti.uniba.it") || !email.contains ("@uniba.it"))) {
+            showError (inputEmail, getString(R.string.error_email));
+        }/*else if (auth.){
+
+        }*/
+            else if (password.isEmpty () || password.length () < 7) {
+            showError (inputPassword, getString(R.string.error_password));
+        } else if (confirmPassword.isEmpty () || !confirmPassword.equals (password)) {
+            showError (inputConfirmPassword, getString(R.string.error_confirm_password));
+        } else {
+            /*LoadingBar.setTitle ("Registration");
+            LoadingBar.setMessage ("please wait check your credentials");
+            LoadingBar.setCanceledOnTouchOutside (false);
+            LoadingBar.show ();*/
+
+
+            auth.createUserWithEmailAndPassword (email,password).addOnCompleteListener (new OnCompleteListener<AuthResult> () {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d ("TAG", "onComplete: entered");
+                    if(task.isSuccessful ()){
+                        Log.d ("TAG", "onComplete: success");
+                        if(email.contains("@studenti.uniba.it")){
+                            Log.d ("TAG", "onComplete: equals studenti uniba");
+                            Toast.makeText (RegisterActivityStart.this, "", Toast.LENGTH_SHORT).show ();
+                            Intent intent=new Intent (RegisterActivityStart.this,RegStudentActivity.class);
+                            intent.putExtra ("email", email);
+                            intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity (intent);
+                        }else if(email.contains("@uniba.it")){
+                            Log.d ("TAG", "onComplete: equals uniba.it");
+                            Toast.makeText (RegisterActivityStart.this, "  Successfully Registration ", Toast.LENGTH_SHORT).show ();
+                            Intent intent=new Intent (RegisterActivityStart.this,RegProfActivity.class);
+                            intent.putExtra ("email",email);
+                            intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity (intent);
+                        }
+                    }
+                    else{
+                        Log.d ("TAG", "onComplete: failed");
+                        Toast.makeText (RegisterActivityStart.this,task.getException ().toString (), Toast.LENGTH_SHORT).show ();
+                    }
+                }
+            });
+        }
+
+
+    }
+    private void showError (EditText input, String s){
+        input.setError (s);
+        input.requestFocus ();
+
+    }
+
+}
+
