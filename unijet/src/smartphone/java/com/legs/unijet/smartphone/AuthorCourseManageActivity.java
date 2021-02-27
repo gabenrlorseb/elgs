7e6b0c27551d0a054111a82fc0b9c7177b8d68d4
anzenrico <enrico.anzelmo@gmail.com>
2021-02-27 12:24:40 +0100

    1) Eliminazione membri corso da parte di un professore

--- unijet/src/smartphone/java/com/legs/unijet/smartphone/AuthorCourseManageActivity.java
+++ unijet/src/smartphone/java/com/legs/unijet/smartphone/AuthorCourseManageActivity.java
@@ -29,6 +29,7 @@ import com.legs.unijet.smartphone.createGroupActivity.CreateGroup;
 import com.legs.unijet.smartphone.createGroupActivity.CreateGroupStart;
 import com.legs.unijet.smartphone.createGroupActivity.MemberCheckListAdapter;
 import com.legs.unijet.smartphone.createGroupActivity.UserChecklistSample;
+import com.legs.unijet.smartphone.groupDetailsActivity.AuthorGroupManageActivity;
 
 import java.util.ArrayList;
 
@@ -58,7 +59,7 @@ public class AuthorCourseManageActivity extends AppCompatActivity {
         Toolbar actionBar = findViewById(R.id.toolbar);
         actionBar.setTitle("Members");
 
-        Bundle args = getIntent().getExtras();
+        final Bundle args = getIntent().getExtras();
 
         passed_names =  (ArrayList<String>) args.getSerializable("groupRecipients");
         authorName = args.getString("author_name");
@@ -105,29 +106,60 @@ public class AuthorCourseManageActivity extends AppCompatActivity {
                 mAdapter.notifyDataSetChanged();
             }
         });
-
         Button fab = findViewById(R.id.remove_button);
-        //fab.setVisibility(View.GONE);
-        //final Animation appearFab = AnimationUtils.loadAnimation(this, R.anim.fab_show);
-        //final Animation disappearFab = AnimationUtils.loadAnimation(this, R.anim.fade_scale_anim_reverse);
-
-        mRecyclerView = findViewById(R.id.possible_members_list);
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent i = new Intent(AuthorCourseManageActivity.this, CourseDetailsActivity.class);
-                    startActivity(i);
-                    finish();
-                }
+                db.child("courses").orderByChild("name").equalTo(args.getString("name")).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                            final String courseUID = childSnapshot.getKey();
+                            Course course;
+                            course = childSnapshot.getValue(Course.class);
+                            ArrayList<String> courseSubscribers = course.getMembers();
+                            //fab.setVisibility(View.GONE);
+                            //final Animation appearFab = AnimationUtils.loadAnimation(this, R.anim.fab_show);
+                            //final Animation disappearFab = AnimationUtils.loadAnimation(this, R.anim.fade_scale_anim_reverse);
+
+                            mRecyclerView = findViewById(R.id.possible_members_list);
+                            if (mAdapter.getCheckedUsers().isEmpty()) {
+                                Toast.makeText(getApplicationContext(), "You Haven't Selected a member", Toast.LENGTH_LONG).show();
+                            } else {
+                                ArrayList<String> removeMail = mAdapter.getCheckedMails();
+                                for (String string: removeMail) {
+                                    courseSubscribers.remove(string);
+
+                                db.child("courses").child(courseUID).child("members").setValue(courseSubscribers);
+                                Intent i = new Intent(AuthorCourseManageActivity.this, CourseDetailsActivity.class);
+                                startActivity(i);
+                                //finish();
+                                }
+                            }
+                        }
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
 
         });
+
+
 
     /* private ArrayList<String> getNamesToBeAdded() {
         ArrayList<String> uuidList;
 
         return uuidList;
     }*/
+    }
+
+    private void removeMembers(){
+
     }
 
     private void populateList() {
