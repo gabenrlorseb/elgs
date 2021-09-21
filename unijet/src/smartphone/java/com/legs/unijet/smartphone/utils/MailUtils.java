97ac6178db35059d2974441bff9b32174b488a66
Gabriele Rizzo <gabriele.rizzo@outlook.com>
2021-09-21 20:13:53 +0200

    MailUtils to smartphone version

---
+++ unijet/src/smartphone/java/com/legs/unijet/smartphone/utils/MailUtils.java
@@ -0,0 +1,36 @@
+package com.legs.unijet.smartphone.utils;
+
+import android.content.Context;
+
+import com.legs.unijet.smartphone.R;
+
+public class MailUtils {
+
+    public static Context context;
+
+
+    public MailUtils(Context context) {
+        MailUtils.context = context;
+    }
+
+    public boolean checkDomainStudents(String url){
+        String[] domains_array = context.getResources().getStringArray(R.array.students_domains);
+        for(String site : domains_array){
+            if(url.contains(site)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDomainTeachers(String url){
+        String[] domains_array = context.getResources().getStringArray(R.array.teaching_domains);
+        for(String site : domains_array){
+            if(url.contains(site)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
